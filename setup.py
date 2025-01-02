d393de51d5786da24b70f828f7bbf73fcb7c5a1b
Akshat-Gupta04 <e22cseu0059@bennett.edu.in>
2025-01-02 22:05:04 +0530

    0.0.0.0

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Food-Delivery Chatbot',
+    version= '0.0.0',
+    author= 'Akshat Gupta',
+    packages= find_packages(),
+    install_requires = []
+
+)
